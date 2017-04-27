6a698c959a311b8b3cc03b7668ba2a1d4f9c1e8b
Luke Rhodes <rhodesy22@gmail.com>
2017-04-27 10:59:38 +1000

    Change persist storage directory

--- helpers/persistentState.js
+++ helpers/persistentState.js
@@ -1,5 +1,10 @@
+const path = require('path');
 const nodePersist = require('node-persist');
-nodePersist.initSync();
+
+var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+home = path.join(home, ".homebridge");
+
+nodePersist.initSync({ dir: `${home}/plugin-persist/homebridge-broadlink-rm` });
 
 const clear = ({ host, name }) => {
   if (!host) host = 'default';
