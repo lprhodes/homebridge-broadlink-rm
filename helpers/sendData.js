d5229f760cdb1e7b390f1f0b981162a14e7d0cf7
Luke Rhodes <rhodesy22@gmail.com>
2018-02-12 09:51:46 +1100

    Improved error logging when sending data to RM devices

--- helpers/sendData.js
+++ helpers/sendData.js
@@ -3,18 +3,18 @@ const assert = require('assert')
 const getDevice = require('./getDevice');
 
 module.exports = ({ host, hexData, log, name, debug }) => {
-  assert(hexData && typeof hexData === 'string', 'HEX value is missing')
+  assert(hexData && typeof hexData === 'string', '\x1b[31m[ERROR]: \x1b[30mHEX value is missing')
 
   // Get the Broadlink device
   const device = getDevice({ host, log })
   if (!device) {
-    if (!host) return log(`${name} sendData (no auto-discovered device found and no "host" option set)`);
+    if (!host) return log(`\x1b[31m[ERROR] \x1b[30m${name} sendData (no auto-discovered device found and no "host" option set)`);
 
-    return log(`${name} sendData (no device found at ${host})`);
+    return log(`\x1b[31m[ERROR] \x1b[30m${name} sendData (no device found at ${host})`);
   }
 
-  if (!device.sendData) return log(`[ERROR] The device at ${device.host.address} (${device.host.macAddress}) doesn't support the sending of IR or RF codes.`);
-  if (hexData.includes('5aa5aa555')) return log('[ERROR] This type of hex code (5aa5aa555...) is no longer valid. Use the included "Learn Code" accessory to find new (decrypted) codes.');
+  if (!device.sendData) return log(`\x1b[31m[ERROR] \x1b[30mThe device at ${device.host.address} (${device.host.macAddress}) doesn't support the sending of IR or RF codes.`);
+  if (hexData.includes('5aa5aa555')) return log('\x1b[31m[ERROR] \x1b[30mThis type of hex code (5aa5aa555...) is no longer valid. Use the included "Learn Code" accessory to find new (decrypted) codes.');
 
   const hexDataBuffer = new Buffer(hexData, 'hex');
   device.sendData(hexDataBuffer, debug);
