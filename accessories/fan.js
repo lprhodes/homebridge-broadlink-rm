8d8706eccf91f014a30afae3b9a04ddd4b9fb170
Luke Rhodes <rhodesy22@gmail.com>
2018-02-12 14:30:52 +1100

    Fixes #248 issue with clockwise/counterClockwise

--- accessories/fan.js
+++ accessories/fan.js
@@ -41,7 +41,7 @@ class FanAccessory extends BroadlinkRMAccessory {
     const services = super.getServices();
     const { config, data, name } = this;
     let { showSwingMode, showRotationDirection, showV1Fan, showV2Fan } = config;
-    const { on, off, swingToggle } = data;
+    const { on, off, clockwise, counterClockwise, swingToggle } = data;
 
     if (showV2Fan !== false) showV2Fan = true
     if (showSwingMode !== false) showSwingMode = true
