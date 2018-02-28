b51c58e1000b40690f8dd718bff4c47781bddb9d
Luke Rhodes <rhodesy22@gmail.com>
2018-02-28 14:47:26 +1100

    Improved support for persistState and resendHexAfterReload

--- accessories/accessory.js
+++ accessories/accessory.js
@@ -5,15 +5,15 @@ const sendData = require('../helpers/sendData');
 class BroadlinkRMAccessory extends HomebridgeAccessory {
 
   constructor (log, config) {
+    if (config.debug) this.debug = true
+
+    config.resendDataAfterReload = config.resendHexAfterReload;
+
     super(log, config);
 
     this.manufacturer = 'Broadlink';
     this.model = 'RM Mini or Pro';
     this.serialNumber = this.host;
-
-    if (config.debug) this.debug = true
-
-    config.resendDataAfterReload = config.resendHexAfterReload;
   }
 
   performSetValueAction ({ host, data, log, name, debug }) {
