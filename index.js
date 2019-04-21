db65c665b547b5f9de75f2646fe4bcbfb73124e8
Alexander Babel <alexander.babel@icloud.com>
2019-04-21 19:30:05 +0200

    Added version check

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const BroadlinkRMPlatform = require('./platform')
+const BroadlinkRMPlatform = require('./platform');
+const compareVersions = require('compare-versions');
 
 module.exports = (homebridge) => {
+  if (compareVersions("0.4.49", homebridge.serverVersion) > 0) {
+    console.log(`[Broadlink RM] The plugin homebridge-broadlink-rm requires HomeBridge v0.4.47 or higher! You have: ${homebridge.serverVersion}.`);
+    process.exit(1);
+  }
   global.Service = homebridge.hap.Service;
   global.Characteristic = homebridge.hap.Characteristic;
 
