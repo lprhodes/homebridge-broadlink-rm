899d1cb2d879571dfb370ecddf98711126f419af
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2019-09-02 14:08:16 +1200

    Update checkForUpdates.js

--- helpers/checkForUpdates.js
+++ helpers/checkForUpdates.js
@@ -12,7 +12,7 @@ const checkForUpdates = () => {
   versionCheck (options, (update, error) => { 
     // if (error) throw error;
     if (update) { 
-      console.log(`\x1b[32m[UPDATE AVAILABLE] \x1b[0mVersion ${update.tag_name} of homebridge-broadlink-rm is available. The release notes can be found here: \x1b[4mhttps://github.com/lprhodes/homebridge-broadlink-rm/releases/\x1b[0m`);
+      console.log(`\x1b[32m[UPDATE AVAILABLE] \x1b[0mVersion ${update.tag_name} of homebridge-broadlink-rm is available. The release notes can be found here: \x1b[4mhttps://github.com/kiwi-cam/homebridge-broadlinkrm-acfile/releases/\x1b[0m`);
     }
   });
 }
