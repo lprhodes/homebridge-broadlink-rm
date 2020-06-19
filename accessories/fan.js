abcd06f93684f331d15fb36d48f970e0e4a30457
Dave Nicolson <david.nicolson@gmail.com>
2020-06-19 16:51:41 +0200

    Fix fan and garage opener door types

--- accessories/fan.js
+++ accessories/fan.js
@@ -1,7 +1,7 @@
 const SwitchAccessory = require('./switch');
 
 class FanAccessory extends SwitchAccessory {
-  serviceType () { return Service.Thermostat }
+  serviceType () { return Service.Fan }
 
   async setSwitchState (hexData, previousValue) {
     const { config, state, serviceManager } = this;
